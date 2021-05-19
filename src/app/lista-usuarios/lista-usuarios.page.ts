d07e93c6966eefa52bfae08b6959b3d1c691a115
Renato <vieirarenato843@gmail.com>
2021-05-19 11:15:03 -0300

    adicionando usuario

---
+++ src/app/lista-usuarios/lista-usuarios.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-lista-usuarios',
+  templateUrl: './lista-usuarios.page.html',
+  styleUrls: ['./lista-usuarios.page.scss'],
+})
+export class ListaUsuariosPage implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+    addusuario() {
+      this.router.navigate([('add-usuario')]);
+    }
+}
