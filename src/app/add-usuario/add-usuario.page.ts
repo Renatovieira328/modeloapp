fbb86cad532143d1d9007a5f9f9c2ec14b8946b9
Renato <vieirarenato843@gmail.com>
2021-05-20 11:53:09 -0300

    banco de dados

--- src/app/add-usuario/add-usuario.page.ts
+++ src/app/add-usuario/add-usuario.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostService } from 'src/service/post.service';
 
 @Component({
   selector: 'app-add-usuario',
@@ -6,26 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-usuario.page.scss'],
 })
 export class AddUsuarioPage implements OnInit {
-id:string="0";
-nome:string="";
-email:string='';
-telefone:string="";
-cidade:string="";
-cpf:string="";
-  constructor() { }
+  id: string = "";
+  nome: string = "";
+  email: string = "";
+  telefone: string = "";
+  cidade: string = "";
+  cpf: string = "";
+
+  constructor(private service: PostService, private router: Router, private actRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.actRoute.params.subscribe((dadosdarota: any) => {
+      this.id = dadosdarota.id;
+      this.nome = dadosdarota.nome;
+      this.email = dadosdarota.email;
+      this.telefone = dadosdarota.telefone;
+      this.cidade = dadosdarota.cidade
+      this.cpf = dadosdarota.cpf
+    })
   }
 
-  cadastrar(){
+  cadastrar() {
     let usuario = {
-      id:this.id,
-      nome:this.nome,
-      email:this.email,
-      telefone:this.telefone,
-      cidade:this.cidade,
-      cpf:this.cpf
+      id: this.id,
+      nome: this.nome,
+      email: this.email,
+      telefone: this.telefone,
+      cidade: this.cidade,
+      cpf: this.cpf
     };
-    console.log(usuario);
+    this.service.dadosApi(usuario, "api.php").subscribe(data => {
+      console.log(data);
+    })
   }
 }
